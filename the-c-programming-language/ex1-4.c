1d7e0718a5979286dad76d19f1792a0c1ca8316c
Mick Piereder <mick.piereder@carruslearn.com>
2024-01-12 15:45:44 -0700

    Added C programming lang exercises 1-1 to 1-4

---
+++ the-c-programming-language/ex1-4.c
@@ -0,0 +1,25 @@
+// “Exercise 1-4. Write a program to print the corresponding Celsius to
+// Fahrenheit table.”
+
+#include <stdio.h>
+
+int main() {
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;   // lower limit of temperature table
+  upper = 300; // upper limit of temperature table
+  step = 30;   // step size
+
+  char border[] = "|-------------------|";
+
+  printf("%s\n| Celsius |   Fahr  |\n%s\n", border, border);
+
+  celsius = lower;
+  while (celsius <= upper) {
+    fahr = celsius * (9.0 / 5.0) + 32;
+    printf("|  %3.0f    | %6.1f  |\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+  printf("%s\n", border);
+}
